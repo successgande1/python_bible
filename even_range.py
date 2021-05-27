d4605a4782aad4a08fd8e57bf20cab9d2728731c
Apollos Gande <60118118+successgande1@users.noreply.github.com>
2021-05-27 02:04:01 +0100

    Practise with List Comprehension using range and conditionals

---
+++ even_range.py
@@ -0,0 +1,4 @@
+#List of even numbers using list comprehension
+even_numbers = range(30)
+
+print([i for i in even_numbers if i % 2 == 0])
---
+++ vowel_list.py
@@ -0,0 +1,3 @@
+#Check for vowels using list comprehension
+original_words = input("Enter Word: ")
+print([i for i in original_words if i in 'aeoui'])
